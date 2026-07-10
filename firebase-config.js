a27bdc74f9c163620850cadcc55315c941ac74d7
yt9495 <yt9495@gmail.com>
2026-07-10 12:24:32 -0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,13 @@
 // Firebase 프로젝트를 만든 뒤 아래 값을 채우고 FIREBASE_ENABLED를 true로 바꾸세요.
 // (Firebase console > 프로젝트 설정 > 일반 > SDK 설정 및 구성에서 값을 복사)
 
-window.FIREBASE_ENABLED = false;
+window.FIREBASE_ENABLED = true;
 
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyA9rk_MsTf2uptvTdCkeT6VXpfsi4h0XxA",
+  authDomain: "hwq-demand.firebaseapp.com",
+  projectId: "hwq-demand",
+  storageBucket: "hwq-demand.firebasestorage.app",
+  messagingSenderId: "228979453814",
+  appId: "1:228979453814:web:7e879de14688abe3755457"
 };
